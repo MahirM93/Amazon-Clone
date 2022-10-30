31a76f38e3d9136fe181e333bb5ac09628e268ea
Mahir Mesanovic <mesanovicmahir@gmail.com>
2022-10-30 09:39:21 +0100

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,45 @@
        
     </head>
     <body>
+        <header>
+            <a href="#"><img src="https://www.bing.com/images/search?view=detailV2&ccid=YdkQGmhB&id=39C8153156298FA98D761C230FA8915757AE29DF&thid=OIP.YdkQGmhB9c2Sr84FeDD9egHaEK&mediaurl=https%3a%2f%2flogos-world.net%2fwp-content%2fuploads%2f2020%2f04%2fAmazon-Logo.png&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.61d9101a6841f5cd92afce057830fd7a%3frik%3d3ymuV1eRqA8jHA%26pid%3dImgRaw%26r%3d0&exph=2160&expw=3840&q=amazon+logo&simid=608032808300188171&FORM=IRPRST&ck=E8A966C96197AA096C5790FBF769E5F8&selectedIndex=0&ajaxhist=0&ajaxserp=0"></a>
+            <div class="map-flex">
+            <i class="fas fa-map-marker"></i>
+                <div>
+                    <span>Deliver to</span>
+                    <span>Germany</span>
+                </div>
+            </div>
+<div class="flex input">
+    <div>
+        <span>All</span>
+        <i class="fas fa-cart-down"></i>
         
+    </div>
+    <input type="text">
+    <i class="fa fa-search"></i>
+    
+</div>
+<div class="flex right">
+        <div class="flex-lang">
+            <img src="" alt="">
+            <i class="fas fa-caret-down"></i>
+</div>
+<div class="sign">
+    <span>Hello,Sign In</span>
+    <div class="flex-ac">
+        <span>Account & List</span>
+        <i class="fas fa-caret-down"></i>
+</div>
+</div>
+<div class="flex cart">
+<i class="fas fa-shopping-cart"></i>
+<span class="ca">Cart</span>
+<p>0</p>
+    
+</div>
+</div> 
+</div>   
+        </header>
     </body>
 </html>
