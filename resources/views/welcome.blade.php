a97aab904b86e6732161197cb1f119838f283c48
Mahir Mesanovic <mesanovicmahir@gmail.com>
2022-10-31 17:28:30 +0100

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
         </div>
         <div class="sign">
             <span>Hello,Sign In</span>
-            <div class="flex-ac">
+            <div class="flex ac">
                 <span>Account & List</span>
                 <i class="fas fa-caret-down"></i>
         </div>
@@ -63,7 +63,198 @@
         </div> 
         </div>  
         </div>
+
+
+
+<!---OVO je bratee moja NOvo nesta--->
+
+<div class="second">
+<div class="second-1">
+    <i class="fas fa-bars"></i>
+    <span>All</span>
+    
+</div>
+<div class="second-2">
+<ul>
+<li>Today's Deal</li>
+<li>Customer Service</li>
+<li>Registry</li>
+<li>Gift Cards</li>
+<li>Sell</li>
+</ul>
+</div>
+
+<div class="second-3">
+    <span>Shop Hallowen Day</span>
+</div>
+</div>
+
+
+
+
+
         </header>
+
+
+        <!----- IDemo dalje za iste pare--->
+
+        <section class="sec-1">
+                <div class="container">
+                    <div class="container-1">
+                        <div class="para">
+                            <p>You ar on amazon.com.You can also shop on Amazon Germany for milions products.<a href="#">Click me to go to amazon.DE</a></p>
+
+                        </div>
+                        <div class="product-comp">
+                            <div class="box">
+                                <h3>Gaming Accesories</h3>
+                                
+                                    <div class="box-a">
+                                        <div>
+                                            <img src="https://m.media-amazon.com/images/I/51w61520FYL._AC_SY450_.jpg"alt="">
+                                            <span>Headsets</span>
+                                        </div>
+                                        <div>
+                                            <img src="https://th.bing.com/th/id/OIP.HJz-5AT9htw490ltEuGNoAHaHa?pid=ImgDet&rs=1"alt="">
+                                            <span>Chair</span>
+                                        </div>
+                                        <div>
+                                            <img src="https://m.media-amazon.com/images/I/71rkjwHMxHL._AC_SL1500_.jpg"alt="">
+                                            <span>Monitor</span>
+                                        </div>
+                                        <div>
+                                            <img src="https://m.media-amazon.com/images/I/71FRAUuWZDL._AC_SL1500_.jpg"alt="">
+                                            <span>Keybord</span>
+                                        </div>
+                                    
+                                
+                               
+                                
+                                    </div>
+                                    
+
+                                    
+                                
+                            </div>
+                            <div class="box">
+                                <h3>Shop By Category</h3>
+                                
+                                    <div class="box-a">
+                                        <div>
+                                            <img src="https://th.bing.com/th/id/R.3fb058e7ae555e9505935bc2dbe0a712?rik=noBucdPg8ppQHA&pid=ImgRaw&r=0"alt="">
+                                            <span>Laptop</span>
+                                        </div>
+                                        <div>
+                                            <img src="https://wstale.com/wp-content/uploads/2018/07/Untitled-design-4-25.jpg"alt="">
+                                            <span>Video Games</span>
+                                        </div>
+                                        <div>
+                                            <img src="https://www.vertbaudet.de/fstrz/r/s/media.vertbaudet.de/Pictures/vertbaudet/168913/3-teiliges-baby-spielzeug-set-motorikspiele-fsc.jpg?width=457?width=1320"alt="">
+                                            <span>Baby</span>
+                                        </div>
+                                        <div>
+                                            <img src="https://th.bing.com/th/id/R.0dc891bf7f835b85e16bdb93ed73bd9b?rik=vkLVwck%2bZ4cq2w&pid=ImgRaw&r=0"alt="">
+                                            <span>Toy & Games</span>
+                                        </div>
+                                    
+                                
+                               
+                                
+                                    </div>
+                                    
+
+                                    
+                                
+                            </div>
+                            <div class="box">
+                                <h3>Gaming Accesories</h3>
+                                
+                                    <div class="box-a">
+                                        <div>
+                                            <img src="https://m.media-amazon.com/images/I/51w61520FYL._AC_SY450_.jpg"alt="">
+                                            <span>Headsets</span>
+                                        </div>
+                                        <div>
+                                            <img src="https://m.media-amazon.com/images/I/51w61520FYL._AC_SY450_.jpg"alt="">
+                                            <span>Headsets</span>
+                                        </div>
+                                        <div>
+                                            <img src="https://m.media-amazon.com/images/I/51w61520FYL._AC_SY450_.jpg"alt="">
+                                            <span>Headsets</span>
+                                        </div>
+                                        <div>
+                                            <img src="https://m.media-amazon.com/images/I/51w61520FYL._AC_SY450_.jpg"alt="">
+                                            <span>Headsets</span>
+                                        </div>
+                                    
+                                
+                               
+                                
+                                    </div>
+                                    
+
+                                    
+                                
+                            </div>
+                            <div class="box">
+                                <h3>Gaming Accesories</h3>
+                                
+                                    <div class="box-a">
+                                        <div>
+                                            <img src="https://m.media-amazon.com/images/I/51w61520FYL._AC_SY450_.jpg"alt="">
+                                            <span>Headsets</span>
+                                        </div>
+                                        <div>
+                                            <img src="https://m.media-amazon.com/images/I/51w61520FYL._AC_SY450_.jpg"alt="">
+                                            <span>Headsets</span>
+                                        </div>
+                                        <div>
+                                            <img src="https://m.media-amazon.com/images/I/51w61520FYL._AC_SY450_.jpg"alt="">
+                                            <span>Headsets</span>
+                                        </div>
+                                        <div>
+                                            <img src="https://m.media-amazon.com/images/I/51w61520FYL._AC_SY450_.jpg"alt="">
+                                            <span>Headsets</span>
+                                        </div>
+                                    
+                                
+                               
+                                
+                                    </div>
+                                    
+
+                                    
+                                
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+                    <div class="slider">
+                        <div class="image-box">
+                            <div class="slide">
+                                <img src="https://m.media-amazon.com/images/I/81M46TyxC1L._SX3000_.jpg"alt=""/>
+                            </div>
+                            <div class="slide">
+                                <img src="https://m.media-amazon.com/images/I/61sH+m18dKL._SX3000_.jpg"alt=""/>
+                            </div>
+                            <div class="slide">
+                                <img src="https://m.media-amazon.com/images/I/6102ZhXZF5L._SX3000_.jpg"alt=""/>
+                            </div>
+                            <div class="slide">
+                                <img src="https://m.media-amazon.com/images/I/71Azk4ywDsL._SX3000_.jpg"alt=""/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+        </section>
+
     </body>
 </html>
 
@@ -268,6 +459,260 @@ padding:5px;
    top:7px;
    left:23px;
 }
+.cart:hover{
+    border:1px solid white;
+    border-radius:3px;
+}
+.logo img:hover{
+    border:1px solid white;
+    border-radius:3px;
+}
+.map:hover{
+    border:1px solid white;
+    border-radius:3px;
+}
+.lang:hover{
+    border:1px solid white;
+    border-radius:3px;
+}
+.sing:hover{
+    border:1px solid white;
+    border-radius:3px;
+}
+.input .fa-search:hover{
+    background-color:#fdae46;
 
+}
+.input div:hover{
+    background-color:rgb(204,202,202);
+    color:black;
+}
+
+
+
+
+
+
+
+.second{
+    width:100%;
+    height:42px;
+    background-color:#232f3e;
+    display:flex;
+    align-items:center;
+    color:white;
+    overflow:hidden;
+}
+.second ul{
+    align-items:center;
+    list-style:none;
+    display:flex;
+    justify-content:flex-start;
+
+}
+.second span{
+    font-size:15px;
+}
+
+.second-1 div i{
+font-size:20px;
+cursor:pointer;
+border:1px solid transparent;
+border-radius:3px;
+}
+
+.second li{
+    padding:5px 10px;
+    font-size:15px;
+    margin:7px 0;
+    cursor:pointer;
+    border:1px solid transparent;
+    border-radius:3px;
+
+
+}
+.second-1{
+    flex:0.06;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+}
+.second-1 div{
+    display:flex;
+    align-items:center;
+    padding-left:5px;
+
+}
+.second-2{
+    display:flex;
+    flex:0.74;
+    justify-content:flex-start;
+
+}
+.second-3{
+    flex:0.2;
+    display:flex;
+    justify-content:flex-end;
+    margin-right:20px;
+    overflow:hidden;
+}
+.second-3 span{
+padding:5px 10px;
+cursor:pointer;
+border:1px solid transparent;
+border-radius:3px;
+}
+.seconf-1 div span{
+    padding:5px;
+    cursor:pointer;
+}
+.second-1 div:hover{
+    border:1px solid white;
+}
+.second-2 li:hover{
+    border:1px solid white;
+}
+.second-3:hover{
+    border:1px solid white;
+}
+.second-1 span{
+    padding:5px 10px;
+    cursor:pointer;
+}
+.second-1 span:hover{
+    border:1px solid white;
+}
+
+
+.sec-1{
+    position:relative;
+    width:70%;
+    height:1190px;
+    box-sizing:border-box;
+    margin-left:15%
+
+}
+.slide img{
+    width:2360px;
+    height:100%;
+}
+.container{
+    position:relative;
+    width:100%;
+    height:auto;
+
+}
+.slider{
+    width:100%;
+    overflow:hidden;
+    position:relative;
+    height:560px;
+}
+.image-box{
+    
+    display:flex;
+    position:absolute;
+    animation:carousel 15s alternate infinite;
+
+}
+@keyframes carousel{
+    0%{
+        left:0;
+    }
+    20%{
+        left:0%;
+    }
+    24%{
+        left:-2360px;
+    }
+    50%{
+        left:-2360px;
+    }
+    54%{
+        left:calc(-2360*2px);
+    }
+    80%{
+        left:calc(-2360*2px);
+    }
+    84%{
+        left:calc(-2360*3px);
+    }
+    100%{
+        left:calc(-2360*3px);
+    }
+}
+
+
+.container-1{
+    position:absolute;
+    z-index:1;
+    top:230px;
+    left:0;
+    width:100%;
+    padding:20px;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    background:linear-gradient(to button,transparent,#eaeded 30%);
+}
+.container-1 .para{
+    background-color:rgb(238,234,234);
+    width:100%;
+    text-align:center;
+
+}
+.container-1{
+    font-size:14px;
+    padding:12px 0;
+}
+.product-comp{
+    width:100%;
+    height:100%;
+    display:flex;
+    box-sizing:border-box;
+    margin:20px 0;
+    justify-content:space-around;
+
+}
+.product-comp .box{
+    width:315px;
+    height:400px;
+    background-color:white;
+
+}
+.box h3{
+    margin-top:20px;
+    margin-left:20px;
+    font-size:20px;
+    margin-bottom:20px;
+    color:rgb(43,42,42);
+}
+.box a{
+    font-size:13px;
+    margin-left:20px;
+    display:block;
+    padding-top:30px;
+}
+.box-a{
+    overflow:hidden;
+    background-color:white;
+}
+.box div img{
+    width:167px;
+    height:120px;
+    cursor:pointer;
+    margin-left:-12px;
+    margin-right:0;
+}
+
+.box-a div{
+    display:inline-block;
+
+}
+.box-a div span{
+    display:block;
+    font-size:13px;
+    margin-left:20px;
+}
 
 </style>
